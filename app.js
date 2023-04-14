25b1b2a0a4b746c638f55a552cc28b03129457dc
Dash10107 <dakshcjain@gmail.com>
2023-04-14 19:28:25 +0530

    Tricking to see secrets for user

--- app.js
+++ app.js
@@ -166,6 +166,7 @@ app.get("/auth/google/secrets",
   app.post("/submit",function (req,res) { 
     const submittedSecret  = req.body.secret;
     const userprofile = req.user.id;
+    console.log(submittedSecret);
     User.findByIdAndUpdate(userprofile,{$push : {secret : submittedSecret}},function(err,foundUser){
         if(err)
         console.log(err);
